d8ca06e541bccf5655910758827f80cc42bd7df8
Paul Barrios <paulx6_2006@hotmail.com>
2012-03-15 17:40:01 +0100

    Funciones terminadas

--- functions.php
+++ functions.php
@@ -44,28 +44,133 @@ function paginacion($pagina){
 	return $result;
 }
 function registro($datos){
-	$insert="INSERT INTO `news`.`users` (`id`, `user_name`, `password`, `mail`, `bio`) VALUES (NULL, '".$datos['user_name']."', '".$datos['password']."', '".$datos['mail']."', '".$datos['bio']."');";
+	$insert="INSERT INTO `news`.`users` (`id`, `user_name`, `password`, `mail`, `bio`) VALUES (NULL, '".$datos['user_name']."', '".$datos['password']."', '".$datos['mail']."', '".$datos['bio']."')";
 	$query=mysql_query($insert);
+	$select="SELECT MAX( `id` ) AS id FROM `users`";
+	$query=mysql_query($select);
+	$row = mysql_fetch_array($query);
+	$id=$row['id'];
+	$insert="INSERT INTO `news`.`badges` (`id`, `user_id`) VALUES (NULL, '".$id."')";
+	$query=mysql_query($insert);
+	$mensaje="El registro se realizo con exito";
+	if ($query==false) {
+		$mensaje= "Este nombre de usuario ya existe.";
+	}
+	return $mensaje;
 }
 
 function login($datos){
-	$select="SELECT * FROM `users` ";
+	$select="SELECT * FROM `users` WHERE `user_name`= '".$datos['user_name']."'";
 	$query=mysql_query($select);
 	$row = mysql_fetch_array($query);
-	if ($datos['user_name']=&&$) {
-		# code...
+	if($row==false){
+		$mensaje="Este usuario no esta registrado.";
+	}elseif ($datos['user_name']==$row['user_name'] && $datos['password']==$row['password']){
+		session_start();
+		$_SESSION['user_name']=$datos['user_name'];
+		$mensaje="Se ha logeado correctamente.";
+	}else{
+		$mensaje="El usuario o la contraseña no son correctos.";
+	}
+	return $mensaje;
+}
+
+function logout(){
+	session_destroy();
+}
+
+function mostrar_perfil($user_id){
+	$select="SELECT * FROM `users` WHERE `user_name`= '".$user_id['user_id']."'";
+	$query=mysql_query($select);
+	$row = mysql_fetch_array($query);
+
+	return $row;
+}
+
+function edit_perfil($datos){
+	$insert="UPDATE `news`.`users` SET 
+	`user_name` = '".$datos['user_name']."',
+	`password` = '".$datos['password']."',
+	`mail` = '".$datos['mail']."',
+	`bio` = '".$datos['bio']."' 
+	WHERE `users`.`id` =".$datos['id'];
+	$query=mysql_query($insert);
+}
+
+function code_pass($datos){
+	$datos['password'] =  md5($datos['password']);
+	return $datos;
+}
+
+function comentar($user_id=0,$article_id,$content){
+	$insert="INSERT INTO `news`.`comments` (`id`, `article_id`, `user_id`, `content`) VALUES (NULL, '".$user_id."', '".$article_id."', '".$content."')";
+	$query=mysql_query($insert);
+}
+
+function votar($article_id,$user_id=0,$ip){
+	$insert="INSERT INTO `news`.`votes` (`id`, `article_id`, `user_id`, `ip`) VALUES (NULL, '".$article_id."', '".$user_id."', '".$ip."')";
+	$query=mysql_query($insert);
+	
+}
+
+function verificacion_votar($article_id,$ip){
+	$select="SELECT * FROM `votes` WHERE `ip` =".$ip." `article_id`= ".$article_id;
+	$query=mysql_query($select);
+	if($query=false){
+		$mensaje="Ya ha votado este articulo.";
 	}
 }
 
-$id_connection=connect($DBHost,$DBUser,$DBPass);
-$_POST['password'] =  md5($_POST['password']);
-registro($_POST);
-desconnect($id_connection);
+function incremnto_columna($columna,$article_id){
+	$insert="UPDATE `news`.`articles` SET `".$columna."` = `".$columna."`+1 WHERE `articles`.`id` =".$article_id;
+	$query=mysql_query($insert);
+}
+
+function salir_portada($article_id){
+	$select="SELECT `total_votes` FROM `articles` WHERE `id` =".$article_id;
+	$query=mysql_query($select);
+	$row = mysql_fetch_array($query);
+	if($row['total_votes']>=10){
+		$insert="UPDATE `news`.`articles` SET `public` = 1 WHERE `articles`.`id` =".$article_id;
+		$query=mysql_query($insert);
+	}
+}
+
+function mostar_articulo($article_id){
+	$select="SELECT * FROM `articles` WHERE `id`= '".$article_id['article_id']."'";
+	$query=mysql_query($select);
+	$row = mysql_fetch_array($query);
+
+	return $row;
+}
+
+function mostar_comentarios($article_id){
+	$select="SELECT * FROM `comments` WHERE `id`= '".$article_id['article_id']."'";
+	$query=mysql_query($select);
+	$row = mysql_fetch_array($query);
+	while ($row = mysql_fetch_array($query)) {
+		$result[]=$row;
+	}
+	return $result;
+}
+
+function mostar_usuarios(){
+	$select="SELECT * FROM `users` INNER JOIN `badges` ON users.id=badges.user_id";
+	$query=mysql_query($select);
+	$row = mysql_fetch_array($query);
+	while ($row = mysql_fetch_array($query)) {
+		$result[]=$row;
+	}
+	return $result;
+}
+
+function add_article($datos){
+	$insert="INSERT INTO `news`.`articles` (`id`, `title`, `description`, `link`, `image`, `user_id`) VALUES (NULL, '".$datos['title']."', '".$datos['description']."', '".$datos['link']."', '".$datos['image']."', '".$datos['user_id']."')";
+	$query=mysql_query($insert);
+}
+
+function badges($user_id,$badge){
+	$insert="UPDATE `news`.`badges` SET `badge".$badge."` = '1' WHERE `user_id`=".$user_id;
+	$query=mysql_query($insert);
+}
 ?>
-<form action="" method="post">
-	<input type="text" name="user_name">
-	<input type="password" name="password">
-	<input type="text" name="mail">
-	<input type="text" name="bio">
-	<input type="submit" value="">
-</form>
